123f9b0248a9b980e0211fe08c0d5023d62ff848
Wojtek Kryscinski <wkryscinski@gmail.com>
2015-01-21 22:45:37 +0100

    Comments added

--- PrintTestHeader.m
+++ PrintTestHeader.m
@@ -1,10 +1,10 @@
 function PrintTestHeader( imageName, a, b, threshold, windowSize )
 %PRINTTESTHEADER funkcja wypisuje informacje o wykonywanym tescie
-%   @param  imageName - 
-%   @param  a - 
-%   @param  b - 
-%   @param  threshold - 
-%   @param  windowSize - 
+%   @param  imageName - nazwa testowanego obrazka
+%   @param  a - wartosc parametru a uzyta przy generowaniu szumu
+%   @param  b - wartosc parametru b uzyta przy generowaniu szumu
+%   @param  threshold - wartosc progu
+%   @param  windowSize - rozmiar okna
     fprintf('* TEST PARAMETERS\n');
     fprintf('Image: %s\na: %.5f\nb: %.5f\nThreshold: %.3f\nWindow Size: %i\n', char(imageName), a, b, threshold, windowSize);
 
--- PrintTestResults.m
+++ PrintTestResults.m
@@ -1,10 +1,8 @@
 function PrintTestResults( estA, estB, duration )
-%PRINTTESTHEADER funkcja wypisuje informacje o wykonywanym tescie
-%   @param  imageName - 
-%   @param  a - 
-%   @param  b - 
-%   @param  threshold - 
-%   @param  windowSize - 
+%PRINTTESTHEADER funkcja wypisuje informacje o wynikach tescie
+%   @param  estA - wyestymowana wartosc a
+%   @param  estB - wyestymowana wartosc b
+%   @param  duration - czas trwania testu
     fprintf('* TEST RESULTS\n');
     fprintf('Estimated a: %.5f\nEstimated b: %.5f\nDuration: %.5f\n', estA, estB, duration);
 
